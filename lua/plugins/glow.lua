beb8a6b745b249896a608dbcb1122c3fb2304a87
Li Haoyuan <1513624626@qq.com>
2025-07-18 21:40:59 +0800

    Add glow.lua, change reform.lua's config

---
+++ lua/plugins/glow.lua
@@ -0,0 +1,5 @@
+require('glow').setup({
+  style = "dark",
+  width_ratio = 0.7,
+  height_ratio = 0.7
+})
--- lua/plugins/reform.lua
+++ lua/plugins/reform.lua
@@ -1,9 +1,20 @@
 -- reform configuration
 
-require("reform").setup({
-  ctrl_enter = false,
-  on_insert_leave = true,
-  auto_enable = true,
-  supported_filetypes = { 'python', 'lua', 'c', 'cpp', 'javascript' }
-})
-
+require("reform").setup(
+  {
+    ctrl_enter = true,
+    on_insert_leave = true,
+    auto_enable = {
+      enabled = true,
+      filetypes = { 'python', 'lua', 'java', 'javascript', 'json', 'actionscript', 'ruby', 'c',
+        'cpp' },
+      exclude_filetypes = {}
+    },
+    formatters = {
+      python = 'autopep8',
+      c = 'clang-format',
+      cpp = 'clang-format'
+    },
+    debug = false
+  }
+)
